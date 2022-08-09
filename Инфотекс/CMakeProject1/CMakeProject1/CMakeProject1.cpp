58c9ba1e44536244ab745b374f1d9874bdc7da3d
Feelan <panfilovich1@gmail.com>
2022-08-09 03:40:58 +0300

    09.08.2022

--- Инфотекс/CMakeProject1/CMakeProject1/CMakeProject1.cpp
+++ Инфотекс/CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -1,88 +1,217 @@
 ﻿#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <inaddr.h>
 #include <locale>
 #include <string>
 #include <stdlib.h>
 #include <cstddef>
 #include <thread>
+#include <mutex>
+#include <vector>
+
+#pragma comment(lib, "ws2_32.lib")
 
 
 using namespace std;
 
-string change_string(string s) {
-	string new_s = "";
+vector<vector<char>> buf;
+vector<vector<char>> message_buf;
+mutex buf_mutex;
 
-	for (int i = 0; i < s.length(); i++) {
+SOCKET ClientSockGlobal;
+bool conn_client = false;
+
+int connecting_to_server() {
+
+	const char SERVER_IP[] = "127.0.0.1";				
+	const short SERVER_PORT_NUM = 1;				
+	const short BUFF_SIZE = 1024;					
+
+	int erStat;										
+
+	in_addr ip_to_num;
+	inet_pton(AF_INET, SERVER_IP, &ip_to_num);
+
+
+
+	WSADATA wsData;
+	erStat = WSAStartup(MAKEWORD(2, 2), &wsData);
+
+	if (erStat != 0) {
+		cout << WSAGetLastError();
+		return 1;
+	}
+
+
+	SOCKET ClientSock = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (ClientSock == INVALID_SOCKET) {
+		closesocket(ClientSock);
+		WSACleanup();
+		return 1;
+	}
+
+	// Establishing a connection to Server
+	sockaddr_in servInfo;
+
+	ZeroMemory(&servInfo, sizeof(servInfo));
+
+	servInfo.sin_family = AF_INET;
+	servInfo.sin_addr = ip_to_num;
+	servInfo.sin_port = htons(SERVER_PORT_NUM);
+
+	erStat = connect(ClientSock, (sockaddr*)&servInfo, sizeof(servInfo));
+
+	if (erStat != 0) {
+		closesocket(ClientSock);
+		WSACleanup();
+		return 1;
+	}
+
+	ClientSockGlobal = ClientSock;
+	conn_client = true;
+	return 0;
+}
+
+void disconnecting() {
+	closesocket(ClientSockGlobal);
+	WSACleanup();
+	conn_client = false;
+}
+
+vector<char> change_string(vector<char> s) {
+	vector<char> new_s;
+
+	for (int i = 0; i < s.size(); i++) {
 		if (s[i] % 2 == 0) {
-			new_s += "KB";
+			new_s.push_back('K');
+			new_s.push_back('B');
 		}
 		else {
-			new_s += s[i];
+			new_s.push_back(s[i]);
 		}
 	}
 	return new_s;
 }
 
-bool check_string(string s) {
-	for (int i = 0; i < s.length(); i++) {
+bool check_string(vector<char> s) {
+	for (int i = 0; i < s.size(); i++) {
 		if (s[i] - '0' < 0 || s[i] - '0' > 9) return false;
 	}
 	return true;
 }
 
-string sort(string s) {
-	for (int i = 0; i < s.length(); i++) {
-		for (int j = 0; j < s.length() - i - 1; j++) {
-			if (s[j] < s[j + 1]) {
+vector<char> sort(vector<char> s) {
+	for (int i = 0; i < s.size(); i++) {
+		for (int j = 0; j < s.size() - i - 1; j++) {
+			if (s[j] < s[j+1]) {
 				int temp = s[j];
-				s[j] = s[j + 1];
-				s[j + 1] = temp;
+				s[j] = s[j+1];
+				s[j+1] = temp;
 			}
 		}
 	}
 	return s;
 }
 
-string func1() {
-	string s;
-	cin >> s;
-	if (s.length() > 64) {
-		cout << "Message length is more than 64 characters!" << endl;
-		system("pause");
-		system("cls");
-	}
-	else if (!check_string(s)) {
-		cout << "The string must contain only numbers!" << endl;
-		system("pause");
-		system("cls");
-	}
-	else {
-		s = sort(s);
-		cout << change_string(s) << endl;
-		system("pause");
-		system("cls");
-		return s;
-	}
+void func1() {
+	
+	string str;
+	cin >> str;
+	vector<char> s(str.begin(), str.end());
 
+	if (s.size() <= 64 && check_string(s)) {
+		s = change_string(sort(s));
+
+		buf_mutex.lock();
+		buf.push_back(s);
+		buf_mutex.unlock();
+	}
 }
 
+vector <char> int_to_vector_char(int num) {
+	vector<char> str_num;
+	str_num.clear();
+	while (num != 0) {
+		str_num.insert(str_num.begin(), char(num % 10 + '0'));
+		num = num / 10;
+	}
+	return str_num;
+}
 
-void func2(string s) {
-	int sum = 0;
-	cout << s << endl;
-	for (int i = 0; i < s.length(); i++) {
-		if (s[i] - '0' >= 0 && s[i] - '0' <= 9) {
-			sum += s[i] - '0';
+void func2() {
+	
+	
+
+	while (!buf.empty()) {
+		vector<char> s;
+
+		buf_mutex.lock();
+		s = buf.back();
+		buf.pop_back();
+		buf_mutex.unlock();
+
+		for (int i = 0; i < s.size(); i++) cout << s[i];
+		cout << endl;
+		int sum = 0;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] - '0' >= 0 && s[i] - '0' <= 9) {
+				sum += s[i] - '0';
+			}
+		}
+
+		vector<char> mess = int_to_vector_char(sum);
+
+		message_buf.push_back(mess);
+		int packet_size;
+		while (!message_buf.empty()) {
+			vector<char> curr_mess = message_buf.back();
+			message_buf.pop_back();
+			Sleep(1000);
+			if (!conn_client) {
+				if (connecting_to_server() == 0) {
+					packet_size = send(ClientSockGlobal, curr_mess.data(), curr_mess.size(), 0);
+					if (packet_size == SOCKET_ERROR) {
+						message_buf.push_back(curr_mess);
+						disconnecting();
+						break;
+					}
+				}
+				else {
+					message_buf.push_back(curr_mess);
+					break;
+				}
+			}
+			else {
+				packet_size = send(ClientSockGlobal, curr_mess.data(), curr_mess.size(), 0);
+				if (packet_size == SOCKET_ERROR) {
+					message_buf.push_back(curr_mess);
+					disconnecting();
+					break;
+				}
+			}
 		}
 	}
-
-
 }
 
 int main() {
-	setlocale(0, "");
-	while (true) {
-		string s = func1();
-		func2(s);
-	}
+	buf.clear();
+
+	thread thread1([&]() {
+		while (true) {
+			func1();
+		}
+		});
+
+	thread thread2([&]() {
+		while (true) {
+			func2();
+		}
+		});
+	
+	thread1.join();
+	thread2.join();
+
 	return 0;
 }
---
+++ Инфотекс/CMakeProject2/CMakeProject2/CMakeProject2.cpp
@@ -0,0 +1,172 @@
+﻿// Server part of Server-Client chat. Developed by Mr_Dezz
+
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <vector>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+void clean_buff(vector<char> buff) {
+	for (int i = 0; i < buff.size(); i++) {
+		if (buff[i] != '\0') {
+			buff[i] = '\0';
+		}
+		else break;
+	}
+}
+
+int check_buff(vector<char> buff) {
+	int len = 0;
+	int num = 0;
+
+	for (int i = 0; i < buff.size(); i++) {
+		if (buff[i] != '\0') {
+			len++;
+			num = num * 10 + (buff[i] - '0');
+		}
+		else break;
+	}
+	cout << " " << num << " ";
+	if (len <= 2) {
+		return 2;
+	}
+	else if (num % 32 != 0) {
+		return 1;
+	}
+	else return 0;
+}
+
+int main(void)
+{
+	const char IP_SERV[] = "127.0.0.1";			
+	const int PORT_NUM = 1;				
+	const short BUFF_SIZE = 1024;
+
+	
+	int erStat;	
+
+	in_addr ip_to_num;
+	erStat = inet_pton(AF_INET, IP_SERV, &ip_to_num);
+
+	if (erStat <= 0) {
+		cout << "Error in IP translation to special numeric format" << endl;
+		return 1;
+	}
+
+
+	WSADATA wsData;
+
+	erStat = WSAStartup(MAKEWORD(2, 2), &wsData);
+
+	if (erStat != 0) {
+		cout << "Error WinSock version initializaion #";
+		cout << WSAGetLastError();
+		return 1;
+	}
+	else
+		cout << "WinSock initialization is OK" << endl;
+
+	SOCKET ServSock = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (ServSock == INVALID_SOCKET) {
+		cout << "Error initialization socket # " << WSAGetLastError() << endl;
+		closesocket(ServSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Server socket initialization is OK" << endl;
+
+
+	sockaddr_in servInfo;
+	ZeroMemory(&servInfo, sizeof(servInfo));	
+
+	servInfo.sin_family = AF_INET;
+	servInfo.sin_addr = ip_to_num;
+	servInfo.sin_port = htons(PORT_NUM);
+
+	erStat = bind(ServSock, (sockaddr*)&servInfo, sizeof(servInfo));
+
+	if (erStat != 0) {
+		cout << "Error Socket binding to server info. Error # " << WSAGetLastError() << endl;
+		closesocket(ServSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Binding socket to Server info is OK" << endl;
+
+	erStat = listen(ServSock, SOMAXCONN);
+
+	if (erStat != 0) {
+		cout << "Can't start to listen to. Error # " << WSAGetLastError() << endl;
+		closesocket(ServSock);
+		WSACleanup();
+		return 1;
+	}
+	else
+		cout << "Listening..." << endl;
+
+
+
+	while (true) {
+		sockaddr_in clientInfo;
+		ZeroMemory(&clientInfo, sizeof(clientInfo));
+
+		int clientInfo_size = sizeof(clientInfo);
+
+		SOCKET ClientConn = accept(ServSock, (sockaddr*)&clientInfo, &clientInfo_size);
+
+		if (ClientConn == INVALID_SOCKET) {
+			cout << "Client detected, but can't connect to a client. Error # " << WSAGetLastError() << endl;
+			closesocket(ServSock);
+			closesocket(ClientConn);
+			WSACleanup();
+			return 1;
+		}
+		else {
+			cout << "Connection to a client established successfully" << endl;
+			char clientIP[22];
+
+			inet_ntop(AF_INET, &clientInfo.sin_addr, clientIP, INET_ADDRSTRLEN);
+
+			cout << "Client connected with IP address " << clientIP << endl;
+
+		}
+
+
+
+		
+
+		while (true) {
+			vector <char> servBuff(BUFF_SIZE), clientBuff(BUFF_SIZE);
+			short packet_size = 0;
+			packet_size = recv(ClientConn, servBuff.data(), servBuff.size(), 0);
+			if (packet_size == SOCKET_ERROR) {
+				break;
+			}
+			cout << "Client's message: ";
+			int ans = check_buff(servBuff);
+			
+			if (ans == 0)
+				cout << servBuff.data() << endl;
+			else if (ans == 1)
+				cout << "ERROR <Message from user is not a multiple of 32>" << endl;
+			else
+				cout << "ERROR <Message from user less than two characters>" << endl;
+
+			clean_buff(servBuff);
+		}
+		
+	}
+
+	
+
+
+	return 0;
+
+}
