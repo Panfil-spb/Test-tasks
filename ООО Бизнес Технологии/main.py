85d18e448949ae312a8b0ab4f1cc27982ca8c272
Feelan <panfilovich1@gmail.com>
2022-09-28 00:10:10 +0300

    28.09.2022

--- ООО Бизнес Технологии/main.py
+++ ООО Бизнес Технологии/main.py
@@ -7,12 +7,9 @@ def sort(mas: list, s: int, f: int) -> None:
 
 
 def merge(mas: list, s: int, q: int, f: int) -> None:
-    mas1 = mas[s:q+1]
-    mas2 = mas[q+1:f+1]
-    print('_____________________________________')
-    print(s, q, f)
-    print(mas1, mas2)
-    print(mas)
+    mas1 = mas[s:q + 1]
+    mas2 = mas[q + 1:f + 1]
+
     k, i, j = s, 0, 0
     while i < len(mas1) and j < len(mas2):
         if mas1[i] < mas2[j]:
@@ -31,19 +28,13 @@ def merge(mas: list, s: int, q: int, f: int) -> None:
         mas[k] = mas2[j]
         k += 1
         j += 1
-    print(mas)
-    print('_____________________________________')
-
-
-
-
-
 
 
 def main():
     mas = [132, 54, 3, 56, 1, 45, 6, 7, 876, 34, 11, 234]
     print(f'no sort: [{" ".join(str(i) for i in mas)}]')
-    sort(mas, 0, len(mas)-1)
+    sort(mas, 0, len(mas) - 1)
     print(f'sort: [{" ".join(str(i) for i in mas)}]')
+
 
 main()
